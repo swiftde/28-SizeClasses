6a9e5a60a9630f12a64c5aecdf733c75b26112fa
BenchR267 <herzog.benjamin@me.com>
2014-09-26 20:11:44 +0200

    Inital Xcode Commit

---
+++ SizeClasses-Tutorial/SizeClasses-Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClasses-Tutorial
+//
+//  Created by Benjamin Herzog on 26.09.14.
+//  Copyright (c) 2014 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
